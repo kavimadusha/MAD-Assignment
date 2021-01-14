8ef004f67f263e9a874b19ac95c5a9364c8ba17c
_.unii.corrn._ <kavii.madusha@gmail.com>
2021-01-15 01:15:31 +0530

    7

--- app/src/main/java/com/example/servicestation/Vehicleowner_reg.java
+++ app/src/main/java/com/example/servicestation/Vehicleowner_reg.java
@@ -6,12 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
