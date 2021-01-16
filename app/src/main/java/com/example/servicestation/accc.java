ae34621697beff0d505db86bd9846a2de919d621
SamaNavi98 <52487780+SamaNavi98@users.noreply.github.com>
2021-01-16 23:17:38 +0530

    commited

---
+++ app/src/main/java/com/example/servicestation/accc.java
@@ -0,0 +1,14 @@
+package com.example.servicestation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class accc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accc);
+    }
+}
