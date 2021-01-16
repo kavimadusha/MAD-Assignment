e4a32f94262b2c58d7149a2ab65243f235c6b222
Kavidu Madusha <56311015+kavimadusha@users.noreply.github.com>
2021-01-16 22:05:20 +0530

    21

--- app/src/main/java/com/example/servicestation/Gardge_info.java
+++ app/src/main/java/com/example/servicestation/Gardge_info.java
@@ -7,11 +7,13 @@ import android.annotation.SuppressLint;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
@@ -22,7 +24,7 @@ public class Gardge_info extends AppCompatActivity {
     private Button summitbtn ;
     private EditText textView24,textView25,textView36,textView37,textView38,textView39;
 
-    private FirebaseAuth authm;
+    private FirebaseAuth authw;
     FirebaseDatabase rootNode;
     DatabaseReference reference;
 
@@ -33,69 +35,42 @@ public class Gardge_info extends AppCompatActivity {
         setContentView(R.layout.activity_gardge_info);
 
 
-           textView24= findViewById(R.id.textView24);
-           textView25= findViewById(R.id.textView25);
-           textView36= findViewById(R.id.textView36);
-           textView37= findViewById(R.id.textView37);
-           textView38= findViewById(R.id.textView38);
-           textView39= findViewById(R.id.textView39);
-           summitbtn= findViewById(R.id.button11);
+        textView24 = findViewById(R.id.textView24);
+        textView25 = findViewById(R.id.textView25);
+        textView36 = findViewById(R.id.textView36);
+        textView37 = findViewById(R.id.textView37);
+        textView38 = findViewById(R.id.textView38);
+        textView39 = findViewById(R.id.textView39);
+        summitbtn = findViewById(R.id.button11);
 
-            summitbtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    startActivity(new Intent(Gardge_info.this,.class));
-                    rootNode = FirebaseDatabase.getInstance();
-                    reference = rootNode.getReference("Garage Information");
+        summitbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Gardge_info.this,.class));
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("Garage Information");
 
-                    String garagename = textView24.getText().toString();
-                    String Description = textView25.getText().toString();
-                    String address = textView36.getText().toString();
-                    String phonenumber = textView37.getText().toString();
-                    String ownerinfo = textView38.getText().toString();
-                    String nofemp = textView39.getText().toString();
+                String garagename = textView24.getText().toString();
+                String Description = textView25.getText().toString();
+                String address = textView36.getText().toString();
+                String phonenumber = textView37.getText().toString();
+                String ownerinfo = textView38.getText().toString();
+                String nofemp = textView39.getText().toString();
+
+                UserHelperClass3 helperClass3 = new UserHelperClass3(garagename, Description, address, phonenumber, ownerinfo, nofemp);
+                reference.child(garagename).setValue(helperClass3);
+
+                if (TextUtils.isEmpty(garagename) || TextUtils.isEmpty(Description) || TextUtils.isEmpty(address) || TextUtils.isEmpty(phonenumber) || TextUtils.isEmpty(ownerinfo) || TextUtils.isEmpty(nofemp)) {
+                    Toast.makeText(Gardge_info.this, "Please fill all the Information", Toast.LENGTH_SHORT).show();
+                } else {
+                    addinfo(garagename, Description, address, phonenumber, ownerinfo, nofemp);
                 }
-            });
-
-
-
-
-        }
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-            super.onActivityResult(requestCode, resultCode, data);
-            if(requestCode==1 && requestCode==RESULT_OK && data!=null && data.getData()!==null)
-            {
-                imaguri=data.getData();
-                img.setImageURI(imguri);
             }
-        }
-    };
-}
-
-    public void addOwners(){
-        String garagename = textView24.getText().toString();
-        String Description = textView25.getText().toString();
-        String address = textView36.getText().toString();
-        String phonenumber = textView37.getText().toString();
-        String ownerinfo = textView38.getText().toString();
-        String nofemp = textView39.getText().toString();
-
-        if(!TextUtils.isEmpty(garagename) && !TextUtils.isEmpty(Description) && !TextUtils.isEmpty(address) && !TextUtils.isEmpty(phonenumber) && !TextUtils.isEmpty(ownerinfo) && !TextUtils.isEmpty(nofemp)){
-
-            owners owners = new owners(garagename,Description,address,phonenumber,ownerinfo,nofemp);
-            databaseReference.garagename.setText("");
-            Description.setText("");
-            address.setText("");
-            phonenumber.setText("");
-            ownerinfo.setText("");
-            nofemp.setText("");
 
 
-        }
-        else {
-            Toast.makeText( context: MainActivity.this, text: "Please fill all the informations", Toast.LENGTH_LONG).show()
-        }
+        });
     }
+            private  void addinfo(String gname,String des,String address, String Pnum, String Owninfo,String noofemp ) {
+
+            }
     }
-}
--- app/src/main/java/com/example/servicestation/UserHelperClass3.java
+++ app/src/main/java/com/example/servicestation/UserHelperClass3.java
@@ -1,6 +1,66 @@
 package com.example.servicestation;
 
 public class UserHelperClass3 {
-    String
+    String Garage_Name,Description,Address,Phone_Number,Owner_Details,No_of_employees;
 
+    public UserHelperClass3(){
+
+    }
+
+    public UserHelperClass3(String garage_Name, String description, String address, String phone_Number, String owner_Details, String no_of_employees) {
+        Garage_Name = garage_Name;
+        Description = description;
+        Address = address;
+        Phone_Number = phone_Number;
+        Owner_Details = owner_Details;
+        No_of_employees = no_of_employees;
+    }
+
+    public String getGarage_Name() {
+        return Garage_Name;
+    }
+
+    public void setGarage_Name(String garage_Name) {
+        Garage_Name = garage_Name;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getPhone_Number() {
+        return Phone_Number;
+    }
+
+    public void setPhone_Number(String phone_Number) {
+        Phone_Number = phone_Number;
+    }
+
+    public String getOwner_Details() {
+        return Owner_Details;
+    }
+
+    public void setOwner_Details(String owner_Details) {
+        Owner_Details = owner_Details;
+    }
+
+    public String getNo_of_employees() {
+        return No_of_employees;
+    }
+
+    public void setNo_of_employees(String no_of_employees) {
+        No_of_employees = no_of_employees;
+    }
 }
