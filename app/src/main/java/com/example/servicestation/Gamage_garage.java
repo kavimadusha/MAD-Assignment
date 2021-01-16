52062a65926a3912ed70c46d6ba553d95eb7bfae
archana-22 <60168184+archana-22@users.noreply.github.com>
2021-01-16 14:30:42 +0530

    Create Gamage_garage.java
    6th commit(Archana)

---
+++ app/src/main/java/com/example/servicestation/Gamage_garage.java
@@ -0,0 +1,43 @@
+package com.example.garageapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Gamage_garage extends AppCompatActivity {
+    private Button Next, Back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gamage_garage);
+
+        Next = (Button) findViewById(R.id.button6);
+        Back = (Button) findViewById(R.id.button5);
+
+        Next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button();
+            }
+        });
+
+        public void Button(){
+            Gamage_garage gamage_garage = new Gamage_garage();
+            gamage_garage.show(getSupportFragmentManager(), "Gamage Garaj")
+        }
+
+        Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Gamage_garage()
+            }
+            public void Gamage_garage(){
+                Intent intent = new Intent(this, shedule_pickup.class);
+                login_Paralel(intent);
+            });
+        }
+}
